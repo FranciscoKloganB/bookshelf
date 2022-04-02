c21d898f9d7bd175d8a25b42997298e6b6c69370
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-04-02 10:55:18 +0100

    feat: log user out and request re-auth by message on 401 status code - closes #4.3

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,3 +1,5 @@
+import * as auth from 'auth-provider'
+
 const apiURL = process.env.REACT_APP_API_URL
 
 function client(endpoint, {token, customHeaders, ...customConfig} = {}) {
@@ -12,11 +14,19 @@ function client(endpoint, {token, customHeaders, ...customConfig} = {}) {
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
     const data = await response.json()
+
+    if (response.status === 401) {
+      await auth.logout()
+      window.location.assign(window.location)
+
+      return Promise.reject({message: 'Please re-authenticate'})
+    }
+
     if (response.ok) {
       return data
-    } else {
-      return Promise.reject(data)
     }
+
+    return Promise.reject(data)
   })
 }
 
