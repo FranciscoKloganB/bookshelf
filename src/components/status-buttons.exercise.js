ca0ceaf77d1dbc2d513aae8091643f7a3141b46c
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-04-02 22:48:21 +0100

    feat: allow book (listItem) to be marked as read/unread and added/removed to list

--- src/components/status-buttons.exercise.js
+++ src/components/status-buttons.exercise.js
@@ -11,7 +11,9 @@ import {
 } from 'react-icons/fa'
 import Tooltip from '@reach/tooltip'
 // 🐨 you'll need useQuery, useMutation, and queryCache from 'react-query'
+import {useQuery, useMutation, queryCache} from 'react-query'
 // 🐨 you'll also need client from 'utils/api-client'
+import {client} from 'utils/api-client'
 import {useAsync} from 'utils/hooks'
 import * as colors from 'styles/colors'
 import {CircleButton, Spinner} from './lib'
@@ -47,14 +49,20 @@ function TooltipButton({label, highlight, onClick, icon, ...rest}) {
   )
 }
 
+const listItemOnSettledCallback = {
+  onSettled: () => queryCache.invalidateQueries('list-items'),
+}
+
 function StatusButtons({user, book}) {
   // 🐨 call useQuery here to get the listItem (if it exists)
   // queryKey should be 'list-items'
   // queryFn should call the list-items endpoint
+  const {data: listItems} = useQuery('list-items', () =>
+    client('list-items', {token: user.token}).then(data => data.listItems),
+  )
 
-  // 🐨 search through the listItems you got from react-query and find the
-  // one with the right bookId.
-  const listItem = null
+  // 🐨 search through the listItems you got from react-query and find the one with the right bookId.
+  const listItem = listItems?.find(item => item.bookId === book.id)
 
   // 💰 for all the mutations below, if you want to get the list-items cache
   // updated after this query finishes the use the `onSettled` config option
@@ -64,13 +72,58 @@ function StatusButtons({user, book}) {
   // the mutate function should call the list-items/:listItemId endpoint with a PUT
   //   and the updates as data. The mutate function will be called with the updates
   //   you can pass as data.
+  const [update] = useMutation(
+    updates =>
+      client(`list-items/${updates.id}`, {
+        data: updates,
+        method: 'PUT',
+        token: user.token,
+      }),
+    listItemOnSettledCallback,
+  )
 
   // 🐨 call useMutation here and assign the mutate function to "remove"
   // the mutate function should call the list-items/:listItemId endpoint with a DELETE
+  const [remove] = useMutation(({id}) =>
+    client(
+      `list-items/${id}`,
+      {
+        method: 'DELETE',
+        token: user.token,
+      },
+      listItemOnSettledCallback,
+    ),
+  )
 
   // 🐨 call useMutation here and assign the mutate function to "create"
   // the mutate function should call the list-items endpoint with a POST
   // and the bookId the listItem is being created for.
+  const [create] = useMutation(
+    ({bookId}) =>
+      client(`list-items`, {
+        data: {
+          bookId,
+        },
+        token: user.token,
+      }),
+    listItemOnSettledCallback,
+  )
+
+  function markAsUnread() {
+    update({id: listItem.id, finishDate: null})
+  }
+
+  function markAsRead() {
+    update({id: listItem.id, finishDate: Date.now()})
+  }
+
+  function removeFromList() {
+    remove({id: listItem.id})
+  }
+
+  function addToList() {
+    create({bookId: book.id})
+  }
 
   return (
     <React.Fragment>
@@ -82,6 +135,7 @@ function StatusButtons({user, book}) {
             // 🐨 add an onClick here that calls update with the data we want to update
             // 💰 to mark a list item as unread, set the finishDate to null
             // {id: listItem.id, finishDate: null}
+            onClick={markAsUnread}
             icon={<FaBook />}
           />
         ) : (
@@ -91,6 +145,7 @@ function StatusButtons({user, book}) {
             // 🐨 add an onClick here that calls update with the data we want to update
             // 💰 to mark a list item as read, set the finishDate
             // {id: listItem.id, finishDate: Date.now()}
+            onClick={markAsRead}
             icon={<FaCheckCircle />}
           />
         )
@@ -100,6 +155,7 @@ function StatusButtons({user, book}) {
           label="Remove from list"
           highlight={colors.danger}
           // 🐨 add an onClick here that calls remove
+          onClick={removeFromList}
           icon={<FaMinusCircle />}
         />
       ) : (
@@ -107,6 +163,7 @@ function StatusButtons({user, book}) {
           label="Add to list"
           highlight={colors.indigo}
           // 🐨 add an onClick here that calls create
+          onClick={addToList}
           icon={<FaPlusCircle />}
         />
       )}
