38ecae544b777f25891b2861e7f5ac1a75f39844
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-04-03 21:47:28 +0100

    refactor: create useBookSearch and use it in discover.js

--- src/hooks/book/use-book-search.js
+++ src/hooks/book/use-book-search.js
@@ -1,3 +1,28 @@
-export function useBookSearch() {
-  throw new Error('Not implemented yet.')
+import {useQuery} from 'react-query'
+import {client} from 'utils/api-client'
+
+import bookPlaceholderSvg from 'assets/book-placeholder.svg'
+
+const loadingBook = {
+  title: 'Loading...',
+  author: 'loading...',
+  coverImageUrl: bookPlaceholderSvg,
+  publisher: 'Loading Publishing',
+  synopsis: 'Loading...',
+  loadingBook: true,
+}
+
+const loadingBooks = Array.from({length: 10}, (v, index) => ({
+  id: `loading-book-${index}`,
+  ...loadingBook,
+}))
+
+export function useBookSearch(query, user) {
+  const queryResult = useQuery(['bookSearch', {query}], () =>
+    client(`books?query=${encodeURIComponent(query)}`, {
+      token: user.token,
+    }).then(data => data.books),
+  )
+
+  return {...queryResult, books: queryResult.data ?? loadingBooks}
 }
