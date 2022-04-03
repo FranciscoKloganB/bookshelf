b3fa8759845a93371f288a6413b3ccec4bab6720
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-04-03 23:37:59 +0100

    perf: add retry and refetch default configurations for react query config provider - closes #6.2

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -3,7 +3,23 @@ import './bootstrap'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
 import {App} from './app'
+import {ReactQueryConfigProvider} from 'react-query'
+
+const queryConfig = {
+  queries: {
+    // If an error occurs on a component which is eventually wrapped in ErrorBoundary, let it handle error
+    useErrorBoundary: true,
+    // Do not query backends when comming back to tab
+    refetchOnWindowFocus: false,
+    retry: (failureCount, error) => error.status !== 404 && failureCount < 3,
+  },
+}
 
 loadDevTools(() => {
-  ReactDOM.render(<App />, document.getElementById('root'))
+  ReactDOM.render(
+    <ReactQueryConfigProvider config={queryConfig}>
+      <App />
+    </ReactQueryConfigProvider>,
+    document.getElementById('root'),
+  )
 })
