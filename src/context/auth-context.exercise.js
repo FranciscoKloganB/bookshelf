be99aabc7f13ec082a75efbd97aa522be514188f
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-04-08 23:07:30 +0100

    feat: add auth context, respective hooks; use in index.js around the App

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -1,2 +1,70 @@
 // 🐨 create and export a React context variable for the AuthContext
 // 💰 using React.createContext
+import * as auth from 'auth-provider'
+import * as React from 'react'
+import {client} from './utils/api-client'
+import {useAsync} from './utils/hooks'
+import {FullPageSpinner, FullPageErrorFallback} from 'components/lib'
+
+const AuthContext = React.createContext()
+AuthContext.displayName = 'AuthContext'
+
+async function getUser() {
+  let user = null
+
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+
+  return user
+}
+
+export function AuthProvider(props) {
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    isSuccess,
+    run,
+    setData,
+    status
+  } = useAsync()
+
+  React.useEffect(() => {
+    run(getUser())
+  }, [run])
+
+  const login = form => auth.login(form).then(user => setData(user))
+  const register = form => auth.register(form).then(user => setData(user))
+  const logout = () => {
+    auth.logout()
+    setData(null)
+  }
+
+  if (isLoading || isIdle) {
+    return <FullPageSpinner />
+  }
+
+  if (isError) {
+    return <FullPageErrorFallback error={error} />
+  }
+
+  if (isSuccess) {
+    const value = {user, login, register, logout}
+    return <AuthContext.Provider value={value} {...props} />
+  }
+
+  throw new Error(`Unhandled status: ${status}`)
+}
+
+export function useAuth() {
+  const context = React.useContext(AuthContext)
+  if (!context) {
+    throw new Error(`useAuth must be used within a <AuthContextProvider />`)
+  }
+  return context
+}
