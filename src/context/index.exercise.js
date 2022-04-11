208fde6c70bb295e2b4feca6b27b1ba6fe3b2861
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2022-04-11 09:15:53 +0100

    feat: colocate global providers - closes #7.3

--- src/context/index.exercise.js
+++ src/context/index.exercise.js
@@ -1,2 +1,25 @@
-// this module doesn't do anything for the exercise. But you'll use this for
+import React from 'react'
+import {AuthProvider} from 'context/auth-context.exercise'
+import {BrowserRouter as Router} from 'react-router-dom'
+import {ReactQueryConfigProvider} from 'react-query'
+
+const queryConfig = {
+  retry(failureCount, error) {
+    if (error.status === 404) return false
+    else if (failureCount < 2) return true
+    else return false
+  },
+  useErrorBoundary: true,
+  refetchAllOnWindowFocus: false,
+}
+
 // the extra credit!
+export function AppProviders({children}) {
+  return (
+    <ReactQueryConfigProvider config={queryConfig}>
+      <Router>
+        <AuthProvider>{children}</AuthProvider>
+      </Router>
+    </ReactQueryConfigProvider>
+  )
+}
