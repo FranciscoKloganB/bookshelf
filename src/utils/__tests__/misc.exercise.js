f9102fa19cfc6039362cacb131c4285f2b607543
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-04-16 21:56:16 +0100

    feat: test formatDate - closes #11.0

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,2 +1,6 @@
-test.todo('formatDate formats the date to look nice')
+import {formatDate} from 'utils/misc'
 
+test('formatDate formats the date to look nice', () => {
+  const actual = formatDate(new Date('1995-12-17T03:24:00'))
+  expect(actual).toEqual('Dec 95')
+})
