1a693cddbf6dfc4df953fc069c612be5a8baf2b6
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-04-17 09:50:09 +0100

    chore: starts Modal testing #12.0

--- src/components/__tests__/modal.exercise.js
+++ src/components/__tests__/modal.exercise.js
@@ -1,10 +1,19 @@
 // 🐨 you're gonna need this stuff:
 // import {Modal, ModalContents, ModalOpenButton} from '../modal'
+import {Modal, ModalContents, ModalOpenButton} from 'component/modal'
 
-test.todo('can be opened and closed')
-// 🐨 render the Modal, ModalOpenButton, and ModalContents
-// 🐨 click the open button
-// 🐨 verify the modal contains the modal contents, title, and label
-// 🐨 click the close button
-// 🐨 verify the modal is no longer rendered
-// 💰 (use `query*` rather than `get*` or `find*` queries to verify it is not rendered)
+/**
+ * We describe Modal, because Contents, Open and Dismiss buttons are children
+ * of Modal on a compound component pattern -- It makes sense to test them
+ * together.
+ */
+describe('Modal', () => {
+  test('can be opened and closed', () => {
+    // 🐨 render the Modal, ModalOpenButton, and ModalContents
+    // 🐨 click the open button
+    // 🐨 verify the modal contains the modal contents, title, and label
+    // 🐨 click the close button
+    // 🐨 verify the modal is no longer rendered
+    // 💰 (use `query*` rather than `get*` or `find*` queries to verify it is not rendered)
+  })
+})
