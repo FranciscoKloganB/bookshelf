5cfab004bdf27e28072593164496019b4aea25ef
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-04-17 10:40:27 +0100

    test: compound modal component opens and closes

--- src/components/__tests__/modal.exercise.js
+++ src/components/__tests__/modal.exercise.js
@@ -1,6 +1,9 @@
 // 🐨 you're gonna need this stuff:
 // import {Modal, ModalContents, ModalOpenButton} from '../modal'
-import {Modal, ModalContents, ModalOpenButton} from 'component/modal'
+import * as React from 'react'
+import {Modal, ModalContents, ModalOpenButton} from '../modal'
+import {render, screen, within} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 /**
  * We describe Modal, because Contents, Open and Dismiss buttons are children
@@ -9,11 +12,37 @@ import {Modal, ModalContents, ModalOpenButton} from 'component/modal'
  */
 describe('Modal', () => {
   test('can be opened and closed', () => {
+    const label = 'Modal Label'
+    const title = 'Modal Title'
+    const content = 'Modal content'
+
     // 🐨 render the Modal, ModalOpenButton, and ModalContents
+    render(
+      <Modal>
+        <ModalOpenButton>
+          <button>open</button>
+        </ModalOpenButton>
+        <ModalContents aria-label={label} title={title}>
+          <p>{content}</p>
+        </ModalContents>
+      </Modal>,
+    )
+
     // 🐨 click the open button
     // 🐨 verify the modal contains the modal contents, title, and label
+    userEvent.click(screen.getByRole('button', {name: /open/i}))
+
+    const modal = screen.getByRole('dialog')
+    expect(modal).toHaveAttribute('aria-label', label)
+
+    const withinModal = within(modal)
+    expect(withinModal.getByText(content)).toBeInTheDocument()
+    expect(withinModal.getByRole('heading', {name: title})).toBeInTheDocument()
+
     // 🐨 click the close button
     // 🐨 verify the modal is no longer rendered
     // 💰 (use `query*` rather than `get*` or `find*` queries to verify it is not rendered)
+    userEvent.click(withinModal.getByRole('button', {name: /close/i}))
+    expect(screen.queryByRole('dialog')).toBe(null)
   })
 })
