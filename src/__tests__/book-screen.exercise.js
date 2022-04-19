ba3d50041f2df91f2df380b3ae85778fdee1518a
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-04-19 21:36:18 +0100

    chore: setup integration test environment for book screen - need to add cases

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -1,36 +1,85 @@
 // 🐨 here are the things you're going to need for this test:
-// import * as React from 'react'
-// import {render, screen, waitFor} from '@testing-library/react'
-// import {queryCache} from 'react-query'
-// import {buildUser, buildBook} from 'test/generate'
-// import * as auth from 'auth-provider'
-// import {AppProviders} from 'context'
-// import {App} from 'app'
+import * as React from 'react'
+import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
+import {queryCache} from 'react-query'
+import {buildUser, buildBook} from 'test/generate'
+import * as auth from 'auth-provider'
+import {AppProviders} from 'context'
+import {App} from 'app'
 
-// 🐨 after each test, clear the queryCache and auth.logout
+describe('Book Screen', () => {
+  // 🐨 create a user using `buildUser`
+  // 🐨 create a book use `buildBook`
+  const user = buildUser()
+  const book = buildBook()
+  const route = `/book/${book.id}`
+  const token = 'token'
 
-test.todo('renders all the book information')
-// 🐨 "authenticate" the client by setting the auth.localStorageKey in localStorage to some string value (can be anything for now)
+  beforeAll(() => {
+    // 🐨 reassign window.fetch to another function and handle the following requests:
+    // - url ends with `/bootstrap`: respond with {user, listItems: []}
+    // - url ends with `/list-items`: respond with {listItems: []}
+    // - url ends with `/books/${book.id}`: respond with {book}
+    // 💰 window.fetch = async (url, config) => { /* handle stuff here*/ }
+    // 💰 return Promise.resolve({ok: true, json: async () => ({ /* response data here */ })})
+    jest.spyOn(window, 'fetch').mockImplementation(async (url, config) => {
+      return Promise.resolve({
+        ok: true,
+        json: async () => {
+          if (url.endsWith('/bootstrap')) {
+            return {
+              user: {
+                ...user,
+                token
+              },
+              listItems: []
+            }
+          }
 
-// 🐨 create a user using `buildUser`
-// 🐨 create a book use `buildBook`
-// 🐨 update the URL to `/book/${book.id}`
-//   💰 window.history.pushState({}, 'page title', route)
-//   📜 https://developer.mozilla.org/en-US/docs/Web/API/History/pushState
+          if (url.endsWith('/list-items')) {
+            return {listItems: []}
+          }
 
-// 🐨 reassign window.fetch to another function and handle the following requests:
-// - url ends with `/bootstrap`: respond with {user, listItems: []}
-// - url ends with `/list-items`: respond with {listItems: []}
-// - url ends with `/books/${book.id}`: respond with {book}
-// 💰 window.fetch = async (url, config) => { /* handle stuff here*/ }
-// 💰 return Promise.resolve({ok: true, json: async () => ({ /* response data here */ })})
+          if (url.endsWith(`/books/${book.id}`)) {
+            return {book}
+          }
 
-// 🐨 render the App component and set the wrapper to the AppProviders
-// (that way, all the same providers we have in the app will be available in our tests)
+          return Promise.reject(
+            new Error(`MUST HANDLE '${url}' with given config: ${config}`),
+          )
+        },
+      })
+    })
 
-// 🐨 use waitFor to wait for the queryCache to stop fetching and the loading
-// indicators to go away
-// 📜 https://testing-library.com/docs/dom-testing-library/api-async#waitfor
-// 💰 if (queryCache.isFetching or there are loading indicators) then throw an error...
+    // 🐨 "authenticate" the client by setting the auth.localStorageKey in localStorage to some string value
+    window.localStorage.setItem(auth.localStorageKey, token)
 
-// 🐨 assert the book's info is in the document
+    // 🐨 update the URL to `/book/${book.id}`
+    //   💰 window.history.pushState({}, 'page title', route)
+    //   📜 https://developer.mozilla.org/en-US/docs/Web/API/History/pushState
+    window.history.pushState({}, 'Test Book Screen', route)
+  })
+
+  afterAll(() => {
+    window.localStorage.removeItem(auth.localStorageKey)
+    window.fetch.mockRestore()
+  })
+
+  afterEach(() => {
+    // 🐨 after each test, clear the queryCache and auth.logout
+    queryCache.clear()
+  })
+
+  test('renders all the book information', async () => {
+    // 🐨 render the App component and set the wrapper to the AppProviders
+    // (that way, all the same providers we have in the app will be available in our tests)
+    render(<App />, {wrapper: AppProviders})
+
+    // 🐨 use waitFor to wait for the queryCache to stop fetching and the loading indicators to go away
+    // 📜 https://testing-library.com/docs/dom-testing-library/api-async#waitfor
+    // 💰 if (queryCache.isFetching or there are loading indicators) then throw an error...
+    await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+    // 🐨 assert the book's info is in the document
+  })
+})
