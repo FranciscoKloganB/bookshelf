030858f7bcb58e5d231fac8a8b67752556f74a2f
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-04-21 08:31:35 +0100

    test: add book screen initial page landing case - closes #13.0

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -1,6 +1,10 @@
 // 🐨 here are the things you're going to need for this test:
 import * as React from 'react'
-import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react'
 import {queryCache} from 'react-query'
 import {buildUser, buildBook} from 'test/generate'
 import * as auth from 'auth-provider'
@@ -87,7 +91,33 @@ describe('Book Screen', () => {
     // 💰 if (queryCache.isFetching or there are loading indicators) then throw an error...
     await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
 
-    screen.debug()
     // 🐨 assert the book's info is in the document
+    expect(screen.getByRole('img', {name: /book cover/i})).toHaveAttribute(
+      'src',
+      book.coverImageUrl,
+    )
+    expect(screen.getByRole('heading', {name: book.title})).toBeInTheDocument()
+    expect(screen.getByText(book.author)).toBeInTheDocument()
+    expect(screen.getByText(book.publisher)).toBeInTheDocument()
+    expect(screen.getByText(book.synopsis)).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('button', {name: /add to list/i}),
+    ).toBeInTheDocument()
+    expect(
+      screen.queryByRole('button', {name: /remove from list/i}),
+    ).not.toBeInTheDocument()
+    expect(
+      screen.queryByRole('button', {name: /mark as read/i}),
+    ).not.toBeInTheDocument()
+    expect(
+      screen.queryByRole('button', {name: /mark as unread/i}),
+    ).not.toBeInTheDocument()
+    expect(
+      screen.queryByRole('textbox', {name: /notes/i}),
+    ).not.toBeInTheDocument()
+
+    expect(screen.queryByRole('radio', {name: /star/i})).not.toBeInTheDocument()
+    expect(screen.queryByLabelText(/start date/i)).not.toBeInTheDocument()
   })
 })
